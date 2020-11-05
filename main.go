7a54cb47fe55570e0f41ff890e36ff8c36e1edfe
Sidouuuux <mirsidahmed@hotmail.fr>
2020-11-05 15:31:41 +0100

    test

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"fmt"
 
 	"rsc.io/quote"
 )
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) HashingBlock() {
+	blockData := bytes.Join([][]byte{b.Hash, b.Data}, []byte{})
+	blockHash := sha256.Sum256(blockData)
+	b.Hash = blockHash[:]
+}
 
 func main() {
 	fmt.Print(quote.Hello())
